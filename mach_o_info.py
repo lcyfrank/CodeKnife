8961df91cf6ddc169c415228c472a9aff00f7011
Frank <lincy1021@163.com>
2018-12-10 15:06:45 +0800

    add `mach_o_info` includes mach-o file structure

---
+++ mach_o_info.py
@@ -0,0 +1,266 @@
+def _parse_int(_bytes):
+    temp_bytes = b''
+    for i in range(len(_bytes)):
+        temp_bytes = _bytes[i: i + 1] + temp_bytes
+        return int(temp_bytes.hex(), 16)
+
+
+def _parse_str(_bytes):
+    return _bytes.decode('utf-8')
+
+
+class MachBase:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        return cls()
+
+    def get_size(self):
+        return 0
+
+
+class MachHeader(MachBase):
+
+    MH_TOTAL_SIZE = 28
+
+    MH_MAGIC_RANGE = (0, 4)
+    MH_CPUTYPE_RANGE = (4, 4)
+    MH_CPUSUBTYPE_RANGE = (8, 4)
+    MH_FILETYPE_RANGE = (12, 4)
+    MH_NCMDS_RANGE = (16, 4)
+    MH_SIZEOFCMDS_RANGE = (20, 4)
+    MH_FLAGS_RANGE = (24, 4)
+
+    def __init__(self):
+        self.magic = 0
+        self.cputype = 0
+        self.cpusubtype = 0
+        self.filetype = 0
+        self.ncmds = 0
+        self.sizeofcmds = 0
+        self.flags = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        mh = cls()
+        mh.magic = _parse_int(_bytes[0:4])
+        mh.cputype = _parse_int(_bytes[4:8])
+        mh.cpusubtype = _parse_int(_bytes[8:12])
+        mh.filetype = _parse_int(_bytes[12:16])
+        mh.ncmds = _parse_int(_bytes[16:20])
+        mh.sizeofcmds = _parse_int(_bytes[20:24])
+        mh.flags = _parse_int(_bytes[24:28])
+        return mh
+
+    def get_size(self):
+        return MachHeader.MH_TOTAL_SIZE
+
+
+class MachHeader64(MachHeader):
+
+    MH_TOTAL_SIZE = 32
+    MH_RESERVED_RANGE = (28, 4)
+
+    def __init__(self):
+        super().__init__()
+        self.reserved = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        mh_64 = super().parse_from_bytes(_bytes)
+        mh_64.reserved = _parse_int(_bytes[28:32])
+        return mh_64
+
+    def get_size(self):
+        return MachHeader64.MH_TOTAL_SIZE
+
+
+class LoadCommand(MachBase):
+
+    LC_TOTAL_SIZE = 8
+    LC_CMD_RANGE = (0, 4)
+    LC_CMD_CMDSIZE = (4, 8)
+
+    LC_SEGMENT = 0x1
+    LC_SEGMENT_64 = 0x19
+
+    def __init__(self):
+        self.cmd = 0
+        self.cmdsize = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        lc = cls()
+        lc.cmd = _parse_int(_bytes[0:4])
+        lc.cmdsize = _parse_int(_bytes[4:8])
+        return lc
+
+    def get_size(self):
+        return LoadCommand.LC_TOTAL_SIZE
+
+
+class SegmentCommand(LoadCommand):
+
+    SC_TOTAL_SIZE = 56
+    SC_SEGNAME_RANGE = (8, 16)
+    SC_VMADDR_RANGE = (24, 4)
+    SC_VMSIZE_RANGE = (28, 4)
+    SC_FILEOFF_RANGE = (32, 4)
+    SC_FILESIZE_RANGE = (36, 4)
+    SC_MAXPROT_RANGE = (40, 4)
+    SC_INITPROT_RANGE = (44, 4)
+    SC_NSECTS_RANGE = (48, 4)
+    SC_FLAGS_RANGE = (52, 4)
+
+    def __init__(self):
+        super().__init__()
+        self.segname = ''
+        self.vmaddr = 0x0
+        self.vmsize = 0
+        self.fileoff = 0
+        self.filesize = 0
+        self.maxprot = 0
+        self.initprot = 0
+        self.nsects = 0
+        self.flags = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        sc = cls()
+        sc.cmd = _parse_int(_bytes[0:4])
+        sc.cmdsize = _parse_int(_bytes[4:8])
+        sc.segname = _parse_str(_bytes[8:24])
+        sc.vmaddr = _parse_int(_bytes[24:28])
+        sc.vmsize = _parse_int(_bytes[28:32])
+        sc.fileoff = _parse_int(_bytes[32:36])
+        sc.filesize = _parse_int(_bytes[36:40])
+        sc.maxprot = _parse_int(_bytes[40:44])
+        sc.initprot = _parse_int(_bytes[44:48])
+        sc.nsects = _parse_int(_bytes[48:52])
+        sc.flags = _parse_int(_bytes[52:56])
+        return sc
+
+    def get_size(self):
+        return SegmentCommand.LC_TOTAL_SIZE
+
+
+class SegmentCommand64(SegmentCommand):
+
+    SC_TOTAL_SIZE = 72
+    SC_VMADDR_RANGE = (24, 8)
+    SC_VMSIZE_RANGE = (32, 8)
+    SC_FILEOFF_RANGE = (40, 8)
+    SC_FILESIZE_RANGE = (48, 8)
+    SC_MAXPROT_RANGE = (56, 4)
+    SC_INITPROT_RANGE = (60, 4)
+    SC_NSECTS_RANGE = (64, 4)
+    SC_FLAGS_RANGE = (68, 4)
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        sc64 = cls()
+        sc64.cmd = _parse_int(_bytes[0:4])
+        sc64.cmdsize = _parse_int(_bytes[4:8])
+        sc64.segname = _parse_str(_bytes[8:24])
+        sc64.vmaddr = _parse_int(_bytes[24:32])
+        sc64.vmsize = _parse_int(_bytes[32:40])
+        sc64.fileoff = _parse_int(_bytes[40:48])
+        sc64.filesize = _parse_int(_bytes[48:56])
+        sc64.maxprot = _parse_int(_bytes[56:60])
+        sc64.initprot = _parse_int(_bytes[60:64])
+        sc64.nsects = _parse_int(_bytes[64:68])
+        sc64.flags = _parse_int(_bytes[68:72])
+        return sc64
+
+    def get_size(self):
+        return SegmentCommand64.LC_TOTAL_SIZE
+
+
+class Section(MachBase):
+
+    S_TOTAL_SIZE = 68
+    S_SECTNAME_RANGE = (0, 16)
+    S_SEGNAME_RANGE = (16, 32)
+    S_ADDR_RANGE = (32, 36)
+    S_SIZE_RANGE = (36, 40)
+    S_OFFSET_RANGE = (40, 44)
+    S_ALIGN_RANGE = (44, 48)
+    S_RELOFF_RANGE = (48, 52)
+    S_NRELOC_RANGE = (52, 56)
+    S_FLAGS_RANGE = (56, 60)
+    S_RESERVED1_RANGE = (60, 64)
+    S_RESERVED2_RANGE = (64, 68)
+
+    def __init__(self):
+        self.sectname = ''
+        self.segname = ''
+        self.addr = 0x0
+        self.size = 0
+        self.offset = 0
+        self.align = 0
+        self.reloff = 0
+        self.nreloc = 0
+        self.flags = 0
+        self.reserved1 = 0
+        self.reserved2 = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        section = cls()
+        section.sectname = _parse_str(_bytes[0:16])
+        section.segname = _parse_str(_bytes[16:32])
+        section.addr = _parse_int(_bytes[32:36])
+        section.size = _parse_int(_bytes[36:40])
+        section.offset = _parse_int(_bytes[40:44])
+        section.align = _parse_int(_bytes[44:48])
+        section.reloff = _parse_int(_bytes[48:52])
+        section.nreloc = _parse_int(_bytes[52:56])
+        section.flags = _parse_int(_bytes[56:60])
+        section.reserved1 = _parse_int(_bytes[60:64])
+        section.reserved2 = _parse_int(_bytes[64:68])
+        return section
+
+    def get_size(self):
+        return Section.S_TOTAL_SIZE
+
+
+class Section64(Section):
+
+    S_TOTAL_SIZE = 80
+    S_ADDR_RANGE = (32, 8)
+    S_SIZE_RANGE = (40, 8)
+    S_OFFSET_RANGE = (48, 4)
+    S_ALIGN_RANGE = (52, 4)
+    S_RELOFF_RANGE = (56, 4)
+    S_NRELOC_RANGE = (60, 4)
+    S_FLAGS_RANGE = (64, 4)
+    S_RESERVED1_RANGE = (68, 4)
+    S_RESERVED1_RANGE = (72, 4)
+    S_RESERVED3_RANGE = (76, 4)
+
+    def __init__(self):
+        super().__init__()
+        self.reserved3 = 0
+
+    @classmethod
+    def parse_from_bytes(cls, _bytes):
+        section = cls()
+        section.sectname = _parse_str(_bytes[0:16])
+        section.segname = _parse_str(_bytes[16:32])
+        section.addr = _parse_int(_bytes[32:40])
+        section.size = _parse_int(_bytes[40:48])
+        section.offset = _parse_int(_bytes[48:52])
+        section.align = _parse_int(_bytes[52:56])
+        section.reloff = _parse_int(_bytes[56:60])
+        section.nreloc = _parse_int(_bytes[60:64])
+        section.flags = _parse_int(_bytes[64:68])
+        section.reserved1 = _parse_int(_bytes[68:72])
+        section.reserved2 = _parse_int(_bytes[72:76])
+        section.reserved3 = _parse_int(_bytes[76:80])
+        return section
+
+    def get_size(self):
+        return Section.S_TOTAL_SIZE
--- static_analysis.py
+++ static_analysis.py
@@ -1,8 +1,9 @@
 from capstone import *
 from capstone.arm64 import *
+from mach_o_info import *
 
-TEST_PATH = './Test'
-# TEST_PATH = './Target/pinduoduo'
+# TEST_PATH = './Test'
+TEST_PATH = './Target/HotPatchDemo'
 
 # Constant
 FA_CPU_TYPE_KEY = 'cputype'
@@ -11,30 +12,35 @@ FA_OFFSET_KEY = 'offset'
 FA_SIZE_KEY = 'size'
 FA_ALIGN_KEY = 'align'
 
-def parse_int_from_bytes(buffer, begin, length, little = True):
-    '''
-    Parse the integer from given bytes
-    > buffer: given bytes
-    > begin: begin point in given bytes
-    > length: length of parsing part of bytes
-    > little: is little end
+
+def parse_int_from_bytes(buffer, begin, length, little=True):
+    '''Parse the integer from given bytes.
+
+    Args:
+        buffer: given bytes
+        begin: begin point in given bytes
+        length: length of parsing part of bytes
+        little: is little end
     '''
     if little:
         temp_buffer = b''
         for i in range(begin, begin + length):
-            temp_buffer = buffer[i : i + 1] + temp_buffer    
+            temp_buffer = buffer[i: i + 1] + temp_buffer
         return int(temp_buffer.hex(), 16)
     return int(buffer[begin: begin + length].hex(), 16)
+
 
 def parse_str_from_bytes(buffer, begin, length):
     return buffer[begin:begin + length].decode('utf-8')
 
-def parse_fat_binary_if_should(buffer):
-    '''
-    Parse the Fat header of Mach-O file
-    > buffer: given bytes
 
-    Reference: https://opensource.apple.com/source/xnu/xnu-792/EXTERNAL_HEADERS/mach-o/fat.h.auto.html
+def parse_fat_binary_if_should(buffer):
+    '''Parse the Fat header of Mach-O file.
+
+    Reference: https://opensource.apple.com/source/xnu/xnu-792/EXTERNAL_HEADERS/mach-o/fath.auto.html
+
+    Args:
+        buffer: given bytes
     '''
     fh_field_size = 4  # the size of each field in fat_header
     fh_number_offset = 4  # the `number` field offset in fat_header
@@ -60,116 +66,143 @@ def parse_fat_binary_if_should(buffer):
         for j in range(5):
             begin = fa_base + fat_num * fa_size + fa_field_size * j
             length = fa_field_size
-            parse_values.append(parse_int_from_bytes(buffer, begin, length, False))
+            parse_values.append(parse_int_from_bytes(
+                buffer, begin, length, False))
         fat_archs.append({
-            FA_CPU_TYPE_KEY : parse_values[0], 
-            FA_CPU_SUBTYPE_KEY : parse_values[1], 
-            FA_OFFSET_KEY : parse_values[2],
-            FA_SIZE_KEY : parse_values[3],
-            FA_ALIGN_KEY : parse_values[4]
+            FA_CPU_TYPE_KEY: parse_values[0],
+            FA_CPU_SUBTYPE_KEY: parse_values[1],
+            FA_OFFSET_KEY: parse_values[2],
+            FA_SIZE_KEY: parse_values[3],
+            FA_ALIGN_KEY: parse_values[4]
         })
     return fat_archs
 
-def parse_text_from_mach(buffer, offset = 0x0):
-    # mach header info
-    mh_ncmds_offset = 16
-    mh_ncmds_size = 4
-    mh_size_32 = 28
-    mh_size_64 = 32
-    # load commander info
-    lc_cmd_offset = 0
-    lc_cmd_size = 4
-    lc_cmdsize_offset = 4
-    lc_cmdsize_size = 4
-    # segment info
-    seg_name_offset = 8
-    seg_name_size = 16
-    seg_nsects_offset_32 = 48
-    seg_nsects_offset_64 = 64
-    seg_nsects_size = 4
-    seg_size_32 = 56
-    seg_size_64 = 72
-    # section info
-    sec_name_offset = 0
-    sec_name_size = 16
-    sec_addr_offset = 32
-    sec_addr_size_32 = 4
-    sec_addr_size_64 = 8
-    sec_size_offset_32 = 36
-    sec_size_offset_64 = 40
-    sec_size_size_32 = 4
-    sec_size_size_64 = 8
-    sec_size_32 = 68
-    sec_size_64 = 80
 
-    is_64_bit = False
+def parse_text_from_mach(buffer, offset=0x0):
+    header = None
     if (buffer.startswith(b'\xce\xfa\xed\xfe')):
         # 32-bit
-        pass
+        mach_header_bytes = buffer[0:MachHeader.MH_TOTAL_SIZE]
+        header = MachHeader.parse_from_bytes(mach_header_bytes)
     elif (buffer.startswith(b'\xcf\xfa\xed\xfe')):
         # 64-bit
-        is_64_bit = True
+        mach_header_bytes = buffer[0:MachHeader64.MH_TOTAL_SIZE]
+        header = MachHeader64.parse_from_bytes(mach_header_bytes)
     else:
         print("Current file is not Mach-O binary")
         return None
-    # parse the number of load commanders
-    LC_CMD_NUMBER = 25 if is_64_bit else 1
-    ncmds = parse_int_from_bytes(buffer, mh_ncmds_offset, mh_ncmds_size)
-    print(ncmds)
-    cmds_pointer = mh_size_64 if is_64_bit else mh_size_32
-    for _ in range(ncmds):
-        cmd = parse_int_from_bytes(buffer, cmds_pointer + lc_cmd_offset, lc_cmd_size)
-        cmdsize = parse_int_from_bytes(buffer, cmds_pointer + lc_cmdsize_offset, lc_cmdsize_size)
-        if LC_CMD_NUMBER == cmd:
-            segname_begin = cmds_pointer + seg_name_offset
-            segname_length = seg_name_size
-            segname = parse_str_from_bytes(buffer, segname_begin, segname_length)
-            if segname.startswith('__TEXT'):
-                nsects_begin = cmds_pointer + (seg_nsects_offset_64 if is_64_bit else seg_nsects_offset_32)
-                nsects_length = seg_nsects_size
-                # parse the number of sections
-                nsects = parse_int_from_bytes(buffer, nsects_begin, nsects_length)
-                sect_base = cmds_pointer + (seg_size_64 if is_64_bit else seg_size_32)
-                for sect_number in range(nsects):
-                    current_sect_addr = sect_base + sect_number * (sec_size_64 if is_64_bit else sec_size_32)
-                    sectname_begin = current_sect_addr + sec_name_offset
-                    sectname_length = sec_name_size
-                    # parse the name of section
-                    sectname = parse_str_from_bytes(buffer, sectname_begin, sectname_length)
-                    if sectname.startswith('__text'):
-                        textbase_begin = current_sect_addr + sec_addr_offset
-                        textbase_length = sec_addr_size_64 if is_64_bit else sec_addr_size_32
-                        textsize_begin = current_sect_addr + (sec_size_offset_64 if is_64_bit else sec_size_offset_32)
-                        textsize_length = sec_size_size_64 if is_64_bit else sec_size_size_32
-                        text_base = parse_int_from_bytes(buffer, textbase_begin, textbase_length)
-                        text_size = parse_int_from_bytes(buffer, textsize_begin, textsize_length)
-                        return (text_base, text_size)
-        cmds_pointer += cmdsize
 
-is_fat_binary = False
-seek_p = 0
+    lc_pointer = header.get_size()
+    for _ in range(header.ncmds):
+        
+        load_cmd_bytes = buffer[lc_pointer:lc_pointer +
+                                LoadCommand.LC_TOTAL_SIZE]
+        load_cmd = LoadCommand.parse_from_bytes(load_cmd_bytes)
 
-mach_o_file = open(TEST_PATH, 'rb')
-mach_o_content_bytes = mach_o_file.read()
-fat_archs = parse_fat_binary_if_should(mach_o_content_bytes)
-machine_codes = []
-if fat_archs == None:
+        if (LoadCommand.LC_SEGMENT != load_cmd.cmd and
+                LoadCommand.LC_SEGMENT_64 != load_cmd.cmd):
+            lc_pointer += load_cmd.get_size()
+            continue
+
+        is_64_bit = True
+        if LoadCommand.LC_SEGMENT == load_cmd.cmd:  # 32-bit
+            load_cmd_bytes = buffer[lc_pointer:lc_pointer +
+                                    SegmentCommand.SC_TOTAL_SIZE]
+            load_cmd = SegmentCommand.parse_from_bytes(load_cmd_bytes)
+            is_64_bit = False
+        else:  # 64-bit
+            load_cmd_bytes = buffer[lc_pointer:lc_pointer +
+                                    SegmentCommand64.SC_TOTAL_SIZE]
+            load_cmd = SegmentCommand64.parse_from_bytes(load_cmd_bytes)
+
+        print(load_cmd.segname)
+        # parse `__TEXT` segment
+        if load_cmd.segname.startswith('__TEXT'):
+            section_pointer = lc_pointer + load_cmd.get_size()
+
+            for __ in range(load_cmd.nsects):
+                section = None
+                if is_64_bit:
+                    section_bytes = buffer[section_pointer:section_pointer +
+                                           Section64.S_TOTAL_SIZE]
+                    section = Section64.parse_from_bytes(section_bytes)
+
+                else:
+                    section_bytes = buffer[section_pointer:section_pointer +
+                                           Section.S_TOTAL_SIZE]
+                    section = Section.parse_from_bytes(section_bytes)
+
+                # parse `__text` section
+                if section.sectname.startswith('__text'):
+
+                    return {
+                        'mach_header': header,
+                        'text_section': section
+                    }
+                section_pointer += section.get_size()
+        lc_pointer += load_cmd.get_size()
+
+
+if __name__ == "__main__":
     is_fat_binary = False
-    (text_base, text_size) = parse_text_from_mach(mach_o_content_bytes)
-    machine_codes.append(mach_o_content_bytes[text_base:text_base + text_size])
-else:
-    is_fat_binary = True
-    for fat_arch in fat_archs:
-        begin = fat_arch[FA_OFFSET_KEY]
-        size = fat_arch[FA_SIZE_KEY]
-        print('begin: %d\nsize: %d' %(begin, size))
-        (text_base, text_size) = parse_text_from_mach(mach_o_content_bytes[begin:begin + size], begin)
-        machine_codes.append(mach_o_content_bytes[text_base:text_base + text_size])
-# 
-print(machine_codes[0])
-model = Cs(CS_ARCH_ARM, CS_MODE_ARM)
-for (address, size, mnemonic, op_str) in model.disasm_lite(machine_codes[0], 0x1000):
-    print("0x%x:\t%s\t%s" %(address, mnemonic, op_str))
+    seek_p = 0
+
+    mach_o_file = open(TEST_PATH, 'rb')
+    mach_o_content_bytes = mach_o_file.read()
+    fat_archs = parse_fat_binary_if_should(mach_o_content_bytes)
+    mach_infos = []
+    if fat_archs == None:
+        is_fat_binary = False
+        parse_result = parse_text_from_mach(mach_o_content_bytes)
+        mach_infos.append(parse_result)
+    else:
+        is_fat_binary = True
+        for fat_arch in fat_archs:
+            begin = fat_arch[FA_OFFSET_KEY]
+            size = fat_arch[FA_SIZE_KEY]
+            parse_result = parse_text_from_mach(
+                mach_o_content_bytes[begin:begin + size], begin)
+            mach_infos.append(parse_result)
+
+    model = None
+    for i in range(len(mach_infos)):
+        mach_info = mach_infos[i]
+        # mach_header = mach_info['mach_header']
+        # text_section = mach_info['text_section']
+
+        # print(mach_header.cputype)
+        # arch = CS_ARCH_ALL
+        # if mach_header.cputype 
+# CS_ARCH_ARM = 0
+# CS_ARCH_ARM64 = 1
+# CS_ARCH_MIPS = 2
+# CS_ARCH_X86 = 3
+# CS_ARCH_PPC = 4
+# CS_ARCH_SPARC = 5
+# CS_ARCH_SYSZ = 6
+# CS_ARCH_XCORE = 7
+# CS_ARCH_MAX = 8
+
+
+        # code = machine_code['machine_code']
+        # print(code)
+        # print(code[len(code) - 5: len(code)].hex())
+        # offset = machine_code['offset']
+        # arch = machine_code['arch']
+        # WARNING!!!
+        # 如果机器码太多，反编译过程会中断，不知道为啥
+        # if arch:
+        # model.arch = CS_ARCH_ARM64
+        # model.mode = CS_MODE_ARM
+        # else:
+        # model.arch = CS_ARCH_ARM
+        # model.mode = CS_MODE_THUMB
+        # for (address, size, mnemonic, op_str) in model.disasm_lite(code, offset):
+            # TODO: 找到函数边界（可以通过 capstone 提供的指令类型）
+            # print(address)
+            # print(mnemonic)
+            # pass
+
 
 # Reference:
 # > https://zhuanlan.zhihu.com/p/24858664
