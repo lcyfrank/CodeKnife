6c4d1307ab009574497a5bde10b0635948bbf649
Frank <lincy1021@163.com>
2018-12-19 21:24:37 +0800

    define the instructions structure to represent instructions

---
+++ inner_instruction.py
@@ -0,0 +1,36 @@
+class MethodStorage:
+
+    _method_instructions = []
+
+    @classmethod
+    def insert_instructions(cls, instructions):
+        cls._method_instructions.append(instructions)
+
+    @classmethod
+    def get_instructions(cls, class_name, method_name):
+        for _method_insn in cls._method_instructions:
+            if (_method_insn.class_name == class_name and
+                _method_insn.method_name == method_name):
+                return _method_insn
+        return None
+
+class MethodInstructions:
+
+    def __init__(self, class_name, method_name):
+        self.class_name = class_name
+        self.method_name = method_name
+        self.instructions = []
+    
+    def insert_instruction(self, instruction):
+        self.instructions.append(instruction)
+        
+
+class Instruction:
+
+    def __init__(self, instruction):
+        self.instruction = instruction
+        self.goto_insns = None
+
+    def goto(self, class_name, method_name):
+        self.goto_insns = (class_name, method_name)
+    
