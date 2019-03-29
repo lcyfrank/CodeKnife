de5e9dac9f5ebcb8a9eca9ff14af6a561979158c
Frank <lincy1021@163.com>
2019-03-29 23:29:27 +0800

    [*] add progress

--- view/home.py
+++ view/home.py
@@ -1,16 +1,62 @@
 import webbrowser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import zipfile
+from basic_analysis import basic_analysis
 
+ALLOWED_EXTENSIONS = {'app', 'ipa'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'upload'
+
+
+_g_file_path = None
+
+
+def filter_file_type(file_name: str):
+    if '.' in file_name and file_name.split('.')[1].lower() in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/analysis')
+def analysis():
+    global _g_file_path
+    if _g_file_path is not None:
+        basic_info = basic_analysis(_g_file_path)
+        _g_file_path = None
+        print(basic_info)
+    return render_template('analysis.html')
+
+
+def extract_from_zip(path):
+    zip_file = zipfile.ZipFile(path)
+    target_name = zip_file.namelist()[0][:-1]
+    zip_file.extractall(app.config['UPLOAD_FOLDER'])
+    return os.path.join(app.config['UPLOAD_FOLDER'], target_name)
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    print(request.files)
-    return 'OK'
+    global _g_file_path
+    if 'file' in request.files:
+        file = request.files['file']
+        file_name = file.filename
+        if filter_file_type(file_name):
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            file.save(file_path)
+            if file_name.split('.')[-1] == 'zip':
+                app_path = extract_from_zip(file_path)
+            else:
+                app_path = file_path
+            _g_file_path = app_path
+            return 'OK'
+    return 'Error'
+
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:5000/')
--- view/static/js/index.js
+++ view/static/js/index.js
@@ -1,9 +1,40 @@
 $(function () {
-    $("#upload-button").click(function () {
+    $("#upload-button").on("click", function () {
         $("#file-input").click();
-    })
+    });
+
+    $("#upload-form").on("submit", function (event) {
+        $("#upload-button").attr("disabled", true).text("上传中");
+        $(".progress").css("display", "block");
+
+        event.preventDefault();
+        let formData = new FormData(this);
+        $.ajax({
+            xhr: function() {
+                let xhr = new XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (e) {
+                    if (e.lengthComputable) {
+                        let precent = Math.round(e.loaded * 100 / e.total);
+                        $(".progress-bar").attr("aria-valuenow", precent).css("width", precent + "%");
+                    }
+                });
+                return xhr;
+            },
+            type: 'POST',
+            url: $("#upload-form").attr('action'),
+            cache: false,
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function (response) {
+            window.location.href = '/analysis';
+            console.log("Upload successfully!");
+        }).fail(function (response) {
+            console.log("Upload failed!");
+        });
+    });
 
     $("#file-input").change(function () {
         $("#upload-form").submit();
     })
-})
+});
